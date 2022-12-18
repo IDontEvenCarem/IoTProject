73381703b05a3ed9ad7bb085f893366e651d6880
Kajetan Owczarek <flash3ch@gmail.com>
2022-12-18 17:45:38 +0100

    Azure connecion, interval config

--- src/config-creator.js
+++ src/config-creator.js
@@ -8,7 +8,7 @@ const fs = require('fs/promises')
 const isPathValid = require("is-valid-path")
 const ConfLib = require("conf").default;
 
-module.exports = async function CreateConfig () {
+module.exports = async function CreateConfig() {
     const SavedAnswers = new ConfLib();
 
     const ora = (await import("ora")).default
@@ -24,18 +24,18 @@ module.exports = async function CreateConfig () {
                 try {
                     opc.is_valid_endpointUrl(input) // this function is really bad
                     return true
-                } 
+                }
                 catch {
                     return "Invalid URL, please provide a valid url";
                 }
             }
         }
-    ], {onCancel() {process.exit(0)}})
+    ], { onCancel() { process.exit(0) } })
 
     SavedAnswers.set('opcua-server-url', resUrl['opcua-server-url'])
 
     const spinner = ora("Connecting to the server").start()
-    
+
     try {
         const opcclient = opc.OPCUAClient.create({
             applicationName: "iot-azure-agent",
@@ -57,25 +57,25 @@ module.exports = async function CreateConfig () {
 
     spinner.stop()
     spinner.start("Searching through devices")
-    
+
     const objects = await session.browse("ns=0;i=85")
 
     spinner.stop()
 
     const early_candidate_devices = objects.references.filter(device => device.nodeId.namespace !== 0)
-    const device_info = await Promise.all(early_candidate_devices.map(async device => ({device, isOk: await IsDeviceOk(session, device.nodeId.toString())})))
+    const device_info = await Promise.all(early_candidate_devices.map(async device => ({ device, isOk: await IsDeviceOk(session, device.nodeId.toString()) })))
 
     const remeberedDevices = SavedAnswers.get('selected-devices') || []
     const resDevices = await prompts({
         name: "selected-devices",
         type: "multiselect",
         message: "Select devices to include in configuration",
-        choices: device_info.map(({device, isOk}) => ({
+        choices: device_info.map(({ device, isOk }) => ({
             title: device.displayName.text,
             disabled: !isOk,
             selected: remeberedDevices.includes(device.nodeId.toString())
         }))
-    }, {onCancel() {process.exit(0)}})
+    }, { onCancel() { process.exit(0) } })
     SavedAnswers.set('selected-devices', resDevices['selected-devices'].map(idx => device_info[idx].device.nodeId.toString()))
 
     const resKeys = await prompts(resDevices['selected-devices'].map(idx => ({
@@ -95,21 +95,51 @@ module.exports = async function CreateConfig () {
         onSubmit(prompt, answer) {
             SavedAnswers.set(`iotkey-${prompt.name}`, answer)
         },
-        onCancel() {process.exit(0)}
+        onCancel() { process.exit(0) }
     })
-    
+
+    const resIntervals = await prompts([
+        {
+            name: 'interval-read',
+            type: 'number',
+            message: 'How often to read from the OPC UA server',
+            initial: SavedAnswers.get("interval-read") || 1000,
+            validate(input) {
+                if (input < 0) {
+                    return "Must be positive"
+                }
+                return true
+            }
+        },
+        {
+            name: 'interval-send',
+            type: 'number',
+            message: 'How often to send data to the Azure cloud',
+            initial: SavedAnswers.get("interval-send") || 5000,
+            validate(input) {
+                if (input < 0) {
+                    return "Must be positive"
+                }
+                return true
+            }
+        }
+    ])
+    SavedAnswers.set('interval-read', resIntervals['interval-read'])
+    SavedAnswers.set('interval-send', resIntervals['interval-send'])
+
     const apps = resDevices['selected-devices'].map(idx => ({
         name: `IoT-Agent-${device_info[idx].device.browseName.name}`,
         script: process.argv[1],
         env: {
-            [`${ENV_PREFIX}_ENDPOINT`]: resUrl['opcua-server-url'], 
-            [`${ENV_PREFIX}_DEVICE`]: device_info[idx].device.nodeId.toString(), 
-            [`${ENV_PREFIX}_CONNECTION_STRING`]: resKeys[idx] 
+            [`${ENV_PREFIX}_ENDPOINT`]: resUrl['opcua-server-url'],
+            [`${ENV_PREFIX}_DEVICE`]: device_info[idx].device.nodeId.toString(),
+            [`${ENV_PREFIX}_CONNECTION_STRING`]: resKeys[idx],
+            [`${ENV_PREFIX}_READ_INTERVAL`]: resIntervals['interval-read'],
+            [`${ENV_PREFIX}_SEND_INTERVAL`]: resIntervals['interval-send']
         },
         max_restarts: 10
     }))
 
-    
     const resFilename = await prompts({
         name: "config-name",
         type: 'text',
@@ -123,10 +153,10 @@ module.exports = async function CreateConfig () {
                 return "Invalid path"
             }
         }
-    }, {onCancel() {process.exit(0)}})
+    }, { onCancel() { process.exit(0) } })
     SavedAnswers.set("config-name", resFilename['config-name'])
 
-    await fs.writeFile(resFilename['config-name'], `module.exports = { apps: ${JSON.stringify(apps)} }`)
+    await fs.writeFile(resFilename['config-name'], `module.exports = { apps: ${JSON.stringify(apps, undefined, 2)} }`)
 
     console.log(chalk.green(`Succesfully generated the ecosystem file ${resFilename['config-name']}`))
 }
--- src/index.js
+++ src/index.js
@@ -14,6 +14,24 @@ const INFO = msg => verbose ? console.log(msg) : { }
 const WARN = msg => console.log(msg)
 const ERR = msg => console.error(msg)
 
+/**
+ * Runs the contained async functions in reverse order when shutting down
+ * @type {Function[]}
+*/
+const cleanup_stack = []
+
+async function do_cleanup() {
+    for (const fn of cleanup_stack.reverse()) {
+        await fn();
+    }
+}
+
+async function do_emergency() {
+    await do_cleanup()
+    process.exitCode = 1
+    process.exit()
+}
+
 import("sudo-block").then(sudoBlock => {
     sudoBlock.default();
      
@@ -30,6 +48,8 @@ import("sudo-block").then(sudoBlock => {
         .option('endpoint', { alias: 'e', type: 'string', description: 'The URL of the OPC UA endpoint' })
         .option('device', { alias: 'd', type: 'string', description: 'Name of the device' })
         .option('connection-string', { alias: 'c', type: 'string', description: 'Azure IoT device connection string' })
+        .option('read-interval', { alias: 'r', type: 'number', description: 'How often should the OPC UA server send data to the agent (in milliseconds)', default: 1000})
+        .option('send-interval', { alias: 's', type: 'number', description: 'How often should the agent send data to Azure (in milliseconds)', default: 5000})
         .demandOption("endpoint", 'You have to provide the OPC UA endpoint')
         .demandOption('connection-string', "You have to provide the Azure IoT connection string")
         .demandOption('device', "You have to provide the ID of the device")
@@ -46,21 +66,14 @@ import("sudo-block").then(sudoBlock => {
     
     
     
-    /**
-     * Runs the contained async functions in reverse order when shutting down
-     * @type {Function[]}
-    */
-const cleanup_stack = []
+
 
 async function Main(args) {
     INFO("Setting up signal handling")
+    process.on('beforeExit', do_cleanup)
     process.addListener('SIGINT', async (signal) => {
         INFO("SIGINT received, cleaning up")
-        for (const fn of cleanup_stack.reverse()) {
-            await fn();
-        }
-        process.exitCode = 1
-        process.exit()
+        await do_emergency();
     })
 
     INFO("Connecting to OPCUA server")
@@ -68,24 +81,19 @@ async function Main(args) {
 
     // setup streams and getseters and methods
     INFO("Creating device interfaces")
-    const interfaces = await CreateInterfaces(session, args.device)
+    const interfaces = await CreateInterfaces(session, args.device, parseInt(args['read-interval']))
 
     // connect to azure
     INFO("Connecting to Azure IoT")
+    const azureInterfaces = await CreateAzureInterfaces(args['connection-string'], interfaces, parseInt(args['send-interval']))
 
     // start sending messages there
-    INFO("Starting operation")
+    // INFO("Starting operation")
 
     // console.log(await interfaces.methods.setProductionRate(50));
     // console.log(await interfaces.methods.emergencyStop());
     // console.log(await interfaces.methods.resetErrorStatus());
-
-    const temp_stream = changes_only(interfaces.telemetryStream.map(read => read.Temperature))
-    temp_stream.subscribe({
-        next(temp) {
-            console.log("temperature:", temp)
-        }
-    })
+    INFO("Ready!")
 }
 
 /**
@@ -118,7 +126,6 @@ async function MakeOPCConnection(endpoint) {
 
 /**
  * @typedef {Object} DeviceReads
- * @property {string} Name
  * @property {0|1} ProductionStatus
  * @property {string} WorkorderId
  * @property {number} ProductionRate
@@ -131,8 +138,9 @@ async function MakeOPCConnection(endpoint) {
 /**
  * @param {opc.ClientSession} session 
  * @param {string} device_id 
+ * @param {number} readInterval
  */
-async function CreateInterfaces(session, device_id) {
+async function CreateInterfaces(session, device_id, readInterval) {
     if (device_id.endsWith("/")) {
         device_id = device_id.substring(0, device_id.length - 1)
     }
@@ -177,8 +185,6 @@ async function CreateInterfaces(session, device_id) {
         .map(([field, read]) => [field, read.value.value])
         .value()
     
-    state.push(["Name", /^[^\;]+;(.+)$/.exec(device_id)[1]]) // add the device name to the object
-
     // store stuff that could be local to the telemetry stream, but because they dont close async, we need them to be closed async
     const varbox = {
         opc_sub: undefined,
@@ -192,7 +198,7 @@ async function CreateInterfaces(session, device_id) {
     const telemetryStream = xs.createWithMemory({
         async start (listener) {
             varbox.opc_sub = await session.createSubscription2({
-                requestedPublishingInterval: 1000,
+                requestedPublishingInterval: readInterval,
                 requestedLifetimeCount: 100,
                 requestedMaxKeepAliveCount: 10,
                 maxNotificationsPerPublish: 100,
@@ -202,7 +208,7 @@ async function CreateInterfaces(session, device_id) {
             varbox.monitored = await varbox.opc_sub.monitorItems(
                 _(node_ids).map(id => ({nodeId: id, attributeId: opc.AttributeIds.Value})).dropRight(2).value(),
                 {
-                    samplingInterval: 1000,
+                    samplingInterval: readInterval,
                     discardOldest: true,
                     queueSize: 10
                 },
@@ -245,9 +251,6 @@ async function CreateInterfaces(session, device_id) {
             const objid = opc.coerceNodeId(device_id)
             const metid = opc.coerceNodeId(device_id+'/EmergencyStop')
 
-            console.log(objid)
-            console.log(metid)
-
             return session.call({
                 objectId: objid,
                 methodId: metid,
@@ -262,4 +265,113 @@ async function CreateInterfaces(session, device_id) {
     }
 
     return {telemetryStream, methods}
+}
+
+/**
+ * 
+ * @param {string} connectionString 
+ * @param {Awaited<ReturnType<CreateInterfaces>>} interfaces
+ * @param {number} sendInterval
+ */
+async function CreateAzureInterfaces(connectionString, interfaces, sendInterval) {
+    const Protocol = require('azure-iot-device-mqtt').Mqtt
+    const Client = require('azure-iot-device').Client
+    const Message = require('azure-iot-device').Message
+
+    const client = Client.fromConnectionString(connectionString, Protocol)
+    client.on('message', ( /** @type {Message} */ msg) => {
+        console.log("Got message: ", msg.data.toString()) // temporary
+    })
+    
+    const connected = await client.open()
+    cleanup_stack.push(() => client.close())
+
+    // Copy telemetry into our own scope, as we might provide changes at differing rates (could use an operator)
+    /** @type {DeviceReads} */
+    let televalue = undefined
+    interfaces.telemetryStream.subscribe({
+        next(reads) {
+            televalue = reads
+        },
+        complete() {
+            ERR("Error - telemetry stream closed unexpectedly.")
+            do_emergency()
+        },
+        error(err) {
+            ERR("Error - telemetry stream experienced an error. Message: " + err)
+            do_emergency()
+        }
+    })
+
+    // device twin setup
+    const twin = await client.getTwin()
+        twin.on('properties.desired.ProductionRate', async (delta) => {
+        await interfaces.methods.setProductionRate(delta)
+    })
+
+    // create a stream of error changes
+    interfaces.telemetryStream.map(read => read.DeviceError).compose(changes_only).subscribe({
+        next(error) {
+            // send D2C message
+            const data = {
+                type: 'error',
+                DeviceError: error
+            }
+            const msg = new Message(JSON.stringify(data))
+            client.sendEvent(msg)
+
+            // update twin
+            twin.properties.reported.update({Error: error, LastErrorDate: Date.now()}, (err) => { if (err) ERR(err)})
+        }
+    })
+
+   
+
+    interfaces.telemetryStream.map(read => read.ProductionRate).compose(changes_only).remember().subscribe({
+        next(ProductionRate) {
+            twin.properties.reported.update({ProductionRate}, (err) => { if (err) ERR(err)})
+        } 
+    })
+
+    // direct methods
+    client.onDeviceMethod("EmergencyStop", async (req, res) => {
+        const opcres = await interfaces.methods.emergencyStop()
+        if (opcres.statusCode.value === 0) {
+            res.send(200)
+        } else {
+            res.send(400, opcres.statusCode.toString())
+        }
+    })
+
+    client.onDeviceMethod("ResetErrorStatus", async (req, res) => {
+        const opcres = await interfaces.methods.resetErrorStatus()
+        if (opcres.statusCode.value === 0) {
+            res.send(200)
+        } else {
+            res.send(400, opcres.statusCode.toString())
+        }
+    })
+
+    client.onDeviceMethod("MaintenanceDone", async (req, res) => {
+        twin.properties.reported.update({LastMaintenanceDate: Date.now()}, (err) => { if (err) ERR(err)})
+    })
+
+    // send telemetry to the cloud
+    function SendTelemetry () {
+        if (televalue !== undefined) {
+            const data = {
+                ... _.omit(televalue, 'DeviceError', 'ProductionRate'),
+                GoodCount: televalue.GoodCount[1],
+                BadCount: televalue.BadCount[1],
+                type: 'telemetry'
+            }
+            const msg = new Message(JSON.stringify(data))
+            client.sendEvent(msg)
+        } else {
+            WARN("OPCUATelemetry was not ready for Azure Sending")
+        }
+
+        setTimeout(SendTelemetry, sendInterval)
+    }
+    setTimeout(SendTelemetry, sendInterval)
 }
